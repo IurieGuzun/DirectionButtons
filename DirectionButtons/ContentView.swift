c6c4ea1e4497d398cce119f82ed7375d85f93973
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-26 14:55:50 -0500

    Try SF Symbols

--- DirectionButtons/ContentView.swift
+++ DirectionButtons/ContentView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+       
+        Button(action: {
+            print("Delete")
+        }) {
+            HStack {
+            Image(systemName: "arrow.up.circle")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(Color.white)
+            Image(systemName: "arrow.up")
+           
+                .background(Color.green)
+                .foregroundColor(Color.white)
+            Image(systemName: "arrow.right.square")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(Color.white)
+            Image("Left")
+                Image("Right")
+                .padding()
+                    .background(Color.green)
+                 .foregroundColor(Color.white)
+            Image("pictureBall")
+//            .resizable()
+                .padding(20)
+                .background(/*@START_MENU_TOKEN@*/Color.orange/*@END_MENU_TOKEN@*/)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                Text( "Delete")
+        }
+        }
     }
 }
 
