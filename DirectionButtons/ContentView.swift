14304184c904c38dd144eaf3d0c46d1e8db2679f
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-26 14:01:59 -0500

    Initial Commit

---
+++ DirectionButtons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DirectionButtons
+//
+//  Created by Iurie Guzun on 2021-01-26.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
